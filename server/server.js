4a30dfabb3e0bcca4d470eb8ca800a1ed2b5be9d
Justin Kilburn <ThatMoonMan@gmail.com>
2025-01-05 16:12:43 -0500

    backend issues

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+console.log(process.env.ATLAS_URI)
+
 
 app.use(cors());
 app.use(express.json());
